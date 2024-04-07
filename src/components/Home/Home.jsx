91cebada30c0235983d5105b198b370425e9a438
ahsan200t <ahsanpekua200@gmail.com>
2024-04-07 22:14:13 +0600

    project setup and navbar done

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>This Home</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/UpdateProfile/UpdateProfile.jsx
@@ -0,0 +1,9 @@
+const UpdateProfile = () => {
+    return (
+        <div>
+            <h1>Up profile</h1>
+        </div>
+    );
+};
+
+export default UpdateProfile;
